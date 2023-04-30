288c46594d73403bdef551573a08676f466f3601
TechsupportOnHold <125736044+TechsupportOnHold@users.noreply.github.com>
2023-04-30 20:07:18 +0100

    uxplay.nix

---
+++ uxplay.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, ... }:
+
+{
+  # Open network ports
+  networking.firewall.allowedTCPPorts = [ 7000 7001 7100 ];
+  networking.firewall.allowedUDPPorts = [ 5353 6000 6001 7011 ];
+
+  # To enable network-discovery
+  services.avahi = {
+    enable = true;
+    nssmdns = true;  # printing
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+      userServices = true;
+      domain = true;
+    };
+  };
+}
