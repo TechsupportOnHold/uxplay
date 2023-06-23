50d477c7728f0a4080cd685310f0d9f4963ddabe
TechsupportOnHold <125736044+TechsupportOnHold@users.noreply.github.com>
2023-06-23 11:28:57 +0100

    Update uxplay.nix
    Adding firewall options

--- uxplay.nix
+++ uxplay.nix
@@ -9,6 +9,7 @@
   services.avahi = {
     enable = true;
     nssmdns = true;  # printing
+    openFirewall = true; # ensuring that firewall ports are open as needed
     publish = {
       enable = true;
       addresses = true;
